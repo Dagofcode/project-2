ae925391b4d34393223665cc5e4e909b8309265b
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-07-11 15:59:13 -0500

    changed the database env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const session = require("express-session");
 const { checkLoggedUser } = require("./middlewares/auth");
 
 mongoose
-  .connect("mongodb://localhost/project-2", { useNewUrlParser: true })
+  .connect(process.env.DB, { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
